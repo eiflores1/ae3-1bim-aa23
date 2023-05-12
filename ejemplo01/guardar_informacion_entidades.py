6d4af3a429c9cd8af8950aba8ee9584c9224e2b3
Eduardo <eduflores98@outlook.es>
2023-05-11 22:12:46 -0500

    agregar

---
+++ ejemplo01/guardar_informacion_entidades.py
@@ -0,0 +1,31 @@
+"""
+    Guarda información en las entidades en la base de datos
+"""
+
+from base_datos import conn
+
+# se usa el objeto Connection y se accede al método cursor
+# para poder realizar las acciones en la base de datos.
+
+cursor = conn.cursor()
+
+# a través de cursor su puede ejecutar comandos SQL mediante el método
+# execute
+
+# Crear una cadena que almacene la sentencia de ingreso de información
+# se recuerda los atributos: nombre, apellido, cedula, edad
+nombre = "Eduardo"
+apellido = "Flores"
+cedula = "1011019123"
+edad = 32
+cadena_sql = """INSERT INTO Autor (nombre, apellido, cedula, edad) \
+VALUES ('%s', '%s', '%s', %d);""" % (nombre, apellido, cedula, edad)
+
+# ejecutar el SQL
+cursor.execute(cadena_sql)
+
+# confirmar los cambios a través del objeto importado de tip Connection
+conn.commit()
+
+# cerrar el enlace a la base de datos (recomendado)
+cursor.close()
